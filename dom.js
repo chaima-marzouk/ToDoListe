5824f88250eb9608a73194e10cb4d01d4f958887
chaima marzouk <marzouk.chaima.892@gmail.com>
2021-12-09 17:24:48 +0100

    add project

--- dom.js
+++ dom.js
@@ -1,5 +1,53 @@
-class DOM {
-    constructor() {
+let nom = document.getElementById('project_name');
+let description = document.getElementById('project_description');
+let paragraphe = document.getElementById('project_title');
+let btnn = document.getElementById('add_project_btn')
+
+
+class Project{
+    constructor(id,nom,description){
         
+        this.nom = nom.value;
+        this.description = description.value;
+        this.id = id();
+    }
+
+    showProject = () => {
+        let newProjct = localStorage.getItem("new Project");
+        paragraphe.innerText = JSON.parse(newProjct);
     }
 }
+
+
+
+
+btnn.addEventListener('click' ,function(){
+    
+    function Counter() {
+        let counter = 0;
+    
+        function IncreaseCounter() {
+            return counter++;
+        };
+    
+        return IncreaseCounter;
+    }
+    
+ let id = Counter();  
+ let project = new Project(id(),nom,description);
+ paragraphe.innerText = project.nom;
+
+ let myProject = {
+    "projectName" : project.nom,
+    "projectDescription" : project.description,
+    "id" : id()
+
+ }
+
+ localStorage.setItem("new Project", JSON.stringify(myProject));
+ console.log(project.showProject);
+})
+
+
+
+
--- task.js
+++
@@ -1,8 +0,0 @@
-class Tasks extends DOM {
-    constructor(titleTask, descriptionTask, dateTask, statusTask) {
-        this.titleTask = titleTask;
-        this.descriptionTask = descriptionTask;
-        this.dateTask = dateTask;
-        this.statusTask = sttusTask;
-    }
-}
