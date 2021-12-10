545aa3b0752e8dff47f3055b0aa8d3cfadf05943
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2021-12-10 01:33:06 +0100

    SomeChanges

--- dom.js
+++ dom.js
@@ -2,4 +2,44 @@ class DOM {
     constructor() {
         
     }
+
 }
+
+    showProject = () => {
+        let newProjct = localStorage.getItem("new Project");
+        paragraphe.innerText = JSON.parse(newProjct);
+    }
+
+
+
+
+
+btnn.addEventListener('click' ,function(){
+    
+    function Counter() {
+        let counter = 0;
+    
+        function IncreaseCounter() {
+            return counter++;
+        };
+    
+        return IncreaseCounter;
+    }
+    
+    let id = Counter();  
+    let project = new Project(id(),nom,description);
+    paragraphe.innerText = project.nom;
+
+    let myProject = {
+        "projectName" : project.nom,
+        "projectDescription" : project.description,
+        "id" : id()
+
+    }
+
+    localStorage.setItem("new Project", JSON.stringify(myProject));
+    console.log(project.showProject);
+})
+
+
+
