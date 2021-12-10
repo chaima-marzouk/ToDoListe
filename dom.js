32ec9a597a64e82fafb9cc2d0de40e8f26c59d06
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2021-12-10 02:22:09 +0100

    SomeModificationHtml&Js

--- dom.js
+++ dom.js
@@ -1,55 +1,89 @@
-let nom = document.getElementById('project_name');
+let nameProject = document.getElementById('project_name');
 let description = document.getElementById('project_description');
-let paragraphe = document.getElementById('project_title');
+let titleProject = document.getElementById('project_title');
 let addProjectBtn = document.getElementById('add_project_btn')
 
 
 class Project{
-    constructor(id,nom,description){
+    constructor(id,name,description){
         
-        this.nom = nom.value;
-        this.description = description.value;
-        this.id = id();
+        this.name = name;
+        this.description = description;
+        this.id = id;
     }
 
+    
 }
 
 
-    showProject = () => {
-        let newProjct = localStorage.getItem("new Project");
-        paragraphe.innerText = JSON.parse(newProjct);
-    }
 
 
 
-addProjectBtn.addEventListener('click' ,function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// addProjectBtn.addEventListener('click' ,function(e){
     
-    function Counter() {
-        let counter = 0;
+//     e.preventDefault();
+//     // function Counter() {
+//     //     let counter = 0;
     
-        function IncreaseCounter() {
-            return counter++;
-        };
+//     //     function IncreaseCounter() {
+//     //         return counter++;
+//     //     };
     
-        return IncreaseCounter;
-    }
+//     //     return IncreaseCounter;
+//     // }
     
 
-    let id = Counter();  
-    let project = new Project(id(),nom,description);
-    paragraphe.innerText = project.nom;
+//     // let id = Counter();  
+//     let project = new Project(nom,description);
+//     paragraphe.innerText = project.nom;
 
-    let myProject = {
-        "projectName" : project.nom,
-        "projectDescription" : project.description,
-        "id" : id()
+//     let myProject = {
+//         "projectName" : project.nom,
+//         "projectDescription" : project.description,
+//         // "id" : id()
 
-    }
+//     }
 
-    localStorage.setItem("new Project", JSON.stringify(myProject));
-    console.log(project.showProject);
+// });
 
-})
+
+
 
 
 
